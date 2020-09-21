93f437a18dcd9d8f243288244fc524cd8def7388
dilna15 <ammudilna2000@gmail.com>
2020-09-21 11:49:16 +0530

    fourth commit login and signup

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ import Modal from 'react-modal';
 const Header=()=>{
     const [header,setHeader]=useState(false);
     const [sign,setSign]=useState(false);
+    const [login,setLogin]=useState(false);
     
 
     const onOpenSign=()=>{
@@ -14,10 +15,14 @@ const Header=()=>{
     const onCloseSign=()=>{
         setSign(false);
     }
+    const onOpenLogin=()=>{
+        setLogin(true);
+    }
+    const onCloseLogin=()=>{
+        setLogin(false);
+    }
 
    
-    
-    
     const changeBackground=()=>{
         if(window.scrollY>80){
             setHeader(true);
@@ -40,7 +45,7 @@ const Header=()=>{
                     <i className="home icon"></i>
                     <a >Sign up</a>
                 </div>
-                <div  onClick={onOpenSign} className="headeritem" >
+                <div  onClick={onOpenLogin} className="headeritem" >
                     <i className="home icon"></i>
                     <a >Login</a>
                 </div>
@@ -80,6 +85,45 @@ const Header=()=>{
                     </div>
                 </div><br/>
                 <div className="ui submit button" style={{width:'30%',marginLeft:'35%',backgroundColor:' darkcyan'}}>Sign up</div>
+                
+
+            </div>
+
+            </Modal>
+
+            <Modal isOpen={login} onRequestClose={onCloseLogin} style={{overlay:{zIndex:'99',backgroundColor:'#b3b3b49f'},content:{marginLeft:'35%',width:'30%',height:'75%'}}} >
+            <center><h1>Login</h1></center><br/>
+            <button class="ui google plus button" style={{marginLeft:'35%'}}>
+                <i class="google  icon"></i>
+                    Google
+            </button><br/><br/>
+            <div class="ui horizontal divider">
+                Or
+            </div>
+            <div className="ui form">
+                <div className="inline fields">
+                    <div className="nine wide field">
+                        <label>Name</label>
+                        <input type="text" placeholder="First Name" style={{marginLeft:'22px'}}/>
+                    </div>
+    
+                    <div className="six wide field">
+                        <input type="text" placeholder="Last Name"/>
+                    </div>
+                </div>
+                <div className="inline fields">
+                    <div className="nine wide field">
+                        <label>Email</label>
+                        <input type="text" placeholder="Email" style={{marginLeft:'23px'}}/>
+                    </div>
+                </div>
+                <div className="inline fields">
+                    <div className="nine wide field">
+                        <label>Password</label>
+                        <input type="text" placeholder="Password"/>
+                    </div>
+                </div><br/>
+                <div className="ui submit button" style={{width:'30%',marginLeft:'35%',backgroundColor:' darkcyan'}}>Login</div>
                 
 
             </div>
--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -3,8 +3,16 @@ import { ProSidebar, Menu, MenuItem, SubMenu, SidebarFooter, SidebarContent, Sid
 import 'react-pro-sidebar/dist/css/styles.css';
 import './sideBar.css';
 import { Link } from 'react-router-dom';
+import Modal from 'react-modal';
 
 class SideBar extends React.Component {
+    state={sign:false};
+    onOpenSign=()=>{
+        this.setState({sign:true});
+    }
+    onCloseSign=()=>{
+        this.setState({sign:false});
+    }
 
     render() {
         const icon = <i className=" small home icon"></i>;
@@ -48,15 +56,53 @@ class SideBar extends React.Component {
                     </SidebarContent>
                     
                     <SidebarFooter style={{ marginBottom:'1.5px' }}>
-                    <div className="marginadj" >
+                    <div className="marginadj" onClick={this.onOpenSign} >
                         <div className="backborder" >
                         <i className=" clr  home icon"></i>
-                        Sign in
+                        Sign up
                         </div>
                     </div>
                     </SidebarFooter>
 
                 </ProSidebar>
+                <Modal isOpen={this.state.sign} onRequestClose={this.onCloseSign} style={{overlay:{zIndex:'99',backgroundColor:'#b3b3b49f'},content:{marginLeft:'35%',width:'30%',height:'75%'}}} >
+                    <center><h1>Sign up</h1></center><br/>
+                    <button class="ui google plus button" style={{marginLeft:'35%'}}>
+                    <i class="google  icon"></i>
+                    Google
+                    </button><br/><br/>
+                <div class="ui horizontal divider">
+                    Or
+                </div>
+                <div className="ui form">
+                <div className="inline fields">
+                    <div className="nine wide field">
+                        <label>Name</label>
+                        <input type="text" placeholder="First Name" style={{marginLeft:'22px'}}/>
+                    </div>
+    
+                    <div className="six wide field">
+                        <input type="text" placeholder="Last Name"/>
+                    </div>
+                </div>
+                <div className="inline fields">
+                    <div className="nine wide field">
+                        <label>Email</label>
+                        <input type="text" placeholder="Email" style={{marginLeft:'23px'}}/>
+                    </div>
+                </div>
+                <div className="inline fields">
+                    <div className="nine wide field">
+                        <label>Password</label>
+                        <input type="text" placeholder="Password"/>
+                    </div>
+                </div><br/>
+                <div className="ui submit button" style={{width:'30%',marginLeft:'35%',backgroundColor:' darkcyan'}}>Sign up</div>
+                
+
+            </div>
+
+            </Modal>
 
             </div>
         );
